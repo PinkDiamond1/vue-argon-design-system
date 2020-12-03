0d4c7df6c820c8007db186563802f367dd1e6be0
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-12-03 19:13:30 +0100

    Carousel now works on IE11. Fix #2

--- vue.config.js
+++ vue.config.js
@@ -20,5 +20,8 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-  }
+  },
+  transpileDependencies: [
+    'bootstrap-vue',
+  ]
 };
